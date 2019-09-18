1cbd16000a000d4e643f17e6695d3802b7a364a8
skullquake <ockert8080@gmail.com>
2019-09-18 11:59:07 +0200

    .

--- src/calbndcrt/scr/a.py
+++ src/calbndcrt/scr/a.py
@@ -2,3 +2,4 @@ import sys
 sys.path.insert(1,'./lib')
 import example
 print(example.add(1,2))
+print(example.sub(1,2))
--- src/calbndcrt/src/app/a.cpp
+++ src/calbndcrt/src/app/a.cpp
@@ -1,17 +1,19 @@
 #include<pybind11/embed.h>
 #include<iostream>
+#include<fstream>
 namespace py=pybind11;
 using namespace py::literals;
 int main(int argc,char** argv){
-	py::scoped_interpreter guard{};
-	py::exec(R"(
-import sys
-sys.path.insert(1,'./lib')
-sys.path.insert(1,'./lib')
-import example
-print(example.add(1,2))
-print(example.sub(1,2))
-)"
-	);
+	if(argc==2){
+		std::ifstream t(argv[1]);
+		std::string src((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
+		py::scoped_interpreter guard{};
+		py::exec(src);
+		return 0;
+	}else{
+		std::cerr<<"Usage: "<<argv[0]<<" PATH"<<std::endl;
+		return -1;
+	}
+
 	return 0;
 }
